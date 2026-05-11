62dcf2339fd7fb827944c4eb7d3601854dfefc18
Siddiq <syedsiddiq0703@gmail.com>
2026-05-11 14:30:19 +0530

    Initial Commit Backend Setup

---
+++ src/main/java/com/example/task_manager/Entity/Task.java
@@ -0,0 +1,50 @@
+package com.example.task_manager.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String description;
+    private boolean completed;
+
+    // Default constructor (required by JPA)
+    public Task(){}
+
+    // Constructor without id (since id is auto-generated)
+    public Task(String description, boolean completed) {
+        this.description = description;
+        this.completed = completed;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+}
